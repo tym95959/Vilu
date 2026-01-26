4ebd6c43fb093206c85921b2879e3b3bf20957cf
tym95959 <tym95959@gmail.com>
2026-01-26 14:12:09 +0500

    Create user1.js

---
+++ user1.js
@@ -0,0 +1,17 @@
+
+const users = [
+  { 
+    username: "Irufan", 
+    name: "Abdulla Irufan", 
+    passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", 
+    Level: "Supervisor",  
+    RCNo: "979"
+  },
+   { 
+    username: "Hassan", 
+    name: "Hassan Nazeem", 
+    passwordHash: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4", 
+    Level: "Supervisor",  
+    RCNo: "884"
+  }
+];
