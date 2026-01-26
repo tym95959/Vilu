9787c5b1e888b7237b095e19790ec9639e81a811
tym95959 <tym95959@gmail.com>
2026-01-26 14:11:42 +0500

    Create resfire.js

---
+++ resfire.js
@@ -0,0 +1,13 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAf_sjwVHG65vKhezpS_L7KC2j0WHIDaWc",
+  authDomain: "leelidc-1f753.firebaseapp.com",
+  projectId: "leelidc-1f753",
+  storageBucket: "leelidc-1f753.firebasestorage.app",
+  messagingSenderId: "43622932335",
+  appId: "1:43622932335:web:a7529bce1f19714687129a",
+  measurementId: "G-3KD6ZYS599"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
